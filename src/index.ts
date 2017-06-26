adc5e95da07e99943556887222bb64ce063d4f8f
e_ntyo <entyo.contact@gmail.com>
2017-06-26 21:47:17 +0900

    :rocket: こういう感じにした http://i.imgur.com/sKy2kKW.png

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,45 @@
 // add styles
-import './style.css'
+const styles = require('./style.css');
 
 // three.js
 import * as THREE from 'three'
 import 'imports-loader?THREE=three!../node_modules/three/examples/js/controls/OrbitControls.js'
 
-// create the scene
 let scene = new THREE.Scene()
 
-// create the camera
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
 let renderer = new THREE.WebGLRenderer()
-
-// Add orbit controll function
-let controls = new THREE.OrbitControls(camera);
-
-// set size
 renderer.setSize(window.innerWidth, window.innerHeight)
-
-// add canvas to dom
 document.body.appendChild(renderer.domElement)
 
-// add axis to the scene
-let axis = new THREE.AxisHelper(10)
+let controls = new THREE.OrbitControls(camera); // Add orbit controll function
 
+let axis = new THREE.AxisHelper(10)
 scene.add(axis)
 
-// add lights
 let light = new THREE.DirectionalLight(0xffffff, 1.0)
-
 light.position.set(100, 100, 100)
-
 scene.add(light)
 
 let light2 = new THREE.DirectionalLight(0xffffff, 1.0)
-
 light2.position.set(-100, 100, -100)
-
 scene.add(light2)
+
+let grid = new THREE.GridHelper(1000, 10, new THREE.Color(0x0000ff));
+scene.add(grid);
+     
+//シーンオブジェクトに追加
+scene.add(grid);     
 
 let material = new THREE.MeshBasicMaterial({
 	color: 0xaaaaaa,
 	wireframe: true
 })
 
-// create a box and add it to the scene
-let box = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), material)
-
-scene.add(box)
-
-box.position.x = 0.5
-box.rotation.y = 0.5
-
-camera.position.x = 5
-camera.position.y = 5
-camera.position.z = 5
+camera.position.x = 0
+camera.position.y = 200
+camera.position.z = 0
 
 camera.lookAt(scene.position)
 
@@ -67,9 +51,78 @@ function animate(): void {
 
 function render(): void {
 	let timer = 0.002 * Date.now()
-	box.position.y = 0.5 + 0.5 * Math.sin(timer)
-	box.rotation.x += 0.1
 	renderer.render(scene, camera)
 }
 
 animate()
+
+// Math
+const deg2rad = degrees => {
+	const kDegreeToRadian = 0.017453292519943295;
+  return degrees * kDegreeToRadian;
+};
+
+// Websocket
+const ws = new WebSocket('ws://localhost:5000');
+
+const removeFromScene = (mesh: THREE.Mesh) => {
+	scene.remove(mesh);
+	mesh.geometry.dispose();
+	mesh.material.dispose();
+	mesh = undefined;
+}
+
+let spheres = [];
+
+ws.onmessage = evt => {
+  let msg = JSON.parse(evt.data);
+
+	const x = Math.cos(deg2rad(msg.degree)) * msg.distance;
+	const y = Math.sin(deg2rad(msg.degree)) * msg.distance;
+
+	// Reset last result
+	if (msg.removeFromScene) {
+		spheres.forEach(s => {
+			removeFromScene(s);
+		});
+		spheres　= [];
+	}
+
+	// Plot point
+	if (x && y) {
+		let sphere = new THREE.Mesh(                                        
+			new THREE.SphereGeometry( 2, 50, 50 ),            
+			new THREE.MeshPhongMaterial({                                      
+				color: 0xFF0000
+			})
+		);
+		scene.add(sphere);      
+		sphere.position.set(x, 0, y); // y to z
+		spheres.push(sphere);
+	}
+
+	// Device status
+	if (msg.ready && msg.speed && msg.rate) {
+		// Already exists
+		if (document.getElementsByClassName(styles.textField).length > 0) {
+			return;
+		}
+		
+		// Create text field
+		const textField = document.createElement('div');
+		textField.classList.add(styles.textField);
+
+		[msg.ready, msg.speed, msg.rate]
+		.map((v, i) => {
+			const p = document.createElement('p');
+			p.textContent = Object.keys(msg)[i] + ': ' + v; // This method is bad. Fix later.
+			return p;
+		})
+		.forEach(v => {
+			textField.appendChild(v);
+		});
+
+		document.body.appendChild(textField);
+	}
+
+};
